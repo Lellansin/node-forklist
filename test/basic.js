cb6e7e139072867035f90b511496789bb0da3dec
Lellansin <lellansin@gmail.com>
2014-09-29 15:21:47 +0800

    Update init function

--- test/basic.js
+++ test/basic.js
@@ -1,5 +1,7 @@
-var ForkList = require('../');
+var fs = require('fs');
+var Path = require('path');
 var underscore = require('underscore');
+var ForkList = require('../');
 
 var path = 'child';
 var num = 4;
@@ -11,8 +13,17 @@ var forks = new ForkList({
     }
 });
 
+var filename = './data/basic.js';
+
+fs.writeFileSync(filename, 'var work_1 = work_2 = work_3 = work_4 = 1;');
+
 for (var i = 0; i < 100; i++) {
-    forks.send('"hello child"', i);
+    forks.send(filename, i);
 }
+
+fs.appendFileSync(filename, 'exports.result = work_1 + work_2 + work_3 + work_4;');
+var generator = require(filename);
+
+console.log('result:', generator.result);
 
 forks.shutdown();
