d1b894dfa916cf9f37bb4d4761a26b0f92606e9a
Lellansin <lellansin@gmail.com>
2014-09-30 16:31:15 +0800

    update tests

--- test/script/write.js
+++ test/script/write.js
@@ -3,7 +3,6 @@ var path = require('path');
 var forkList = require('../../');
 
 forkList.proc(function(data_file) {
-	console.log('[work]', this.workid);
 	var d = 'work_' + this.workid + ' += 1;\n';
 	fs.appendFileSync(data_file, d);
 });
