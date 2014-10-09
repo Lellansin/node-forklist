9d7374a6219eecde281efab4a80e460a28a7cbf2
Lellansin <lellansin@gmail.com>
2014-10-09 13:54:30 +0800

    add send data type test.

--- test/script/read.js
+++ test/script/read.js
@@ -1,5 +1,5 @@
 var forkList = require('../../');
 
-forkList.proc(function(data1) {
-    console.log('This is work:', this.workid, 'recv data1:', data1);
+forkList.proc(function(data1, data2) {
+    console.log('This is work:', this.workid, 'recv data1:', data1, 'recv data2:', data2);
 });
